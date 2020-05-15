11162dceb4ca5ef196311304c120f6b5c2e03fac
Daniel <daniel.freitas@treevia.com.br>
2020-05-14 22:35:28 -0300

    Adicionado EF Core no projeto e a criacao do Contexto para o BD

---
+++ backend/codenation.stone.data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using codenation.stone.data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace codenation.stone.data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Log> Logs { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
