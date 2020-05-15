e23c4c25872044b2e1cd2b59cbc374104463e486
Daniel <daniel.freitas@treevia.com.br>
2020-05-14 22:58:33 -0300

    Adicionado a interface IRepository e inicio da implementação do Repository

---
+++ backend/codenation.stone.data/Repository/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace codenation.stone.data.Repository
+{
+    interface IRepository
+    {
+        public T Get<T>();
+        public List<T> GetAll<T>();
+        public void Create<T>(T Model);
+        public void Update<T>(Guid Id, T Model);
+        public void Delete(Guid Id);
+    }
+}
---
+++ backend/codenation.stone.data/Repository/Repository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace codenation.stone.data.Repository
+{
+    public class Repository : IRepository
+    {
+        public void Create<T>(T Model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Get<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> GetAll<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update<T>(Guid Id, T Model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
